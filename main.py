b791d7ec929a856e5103e1f3e8c7780b8242e035
Arthur-lewyin <159451233+Arthur-lewyin@users.noreply.github.com>
2024-09-04 17:12:04 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+set1 = set([])
+set2 = set([])
+
+set_1 = int(input("write a list of numbers for set 1 : "))
+set_2 = int(input("write a list of numbers for set 2 : "))
+
+set1.add(set_1)
+set2.add(set_2)
+
+print(set1)
+print(set2)
+
+print(set1|set2)
